b26cf76df8ddd066eb6702f728d588715e99fb4a
tccoin <junzhewu@umich.edu>
2022-11-15 15:25:25 -0500

    update backend & noisy traj tools

--- gtsam_backend.py
+++ gtsam_backend.py
@@ -12,30 +12,44 @@ class GTSAM_Backend():
     def __init__(self, frontend):
         self.frontend = frontend
     
-    def optimize(self):
+    def optimize(self, use_smart_factor=True):
+        # graph
         graph = gtsam.NonlinearFactorGraph()
         initial_estimate = gtsam.Values()
+
+        # factors
+        smart_factors = {}
 
         # camera and noise
         fx, fy, cx, cy = self.frontend.camera
         K = gtsam.Cal3_S2(fx, fy, 0.0, cx, cy)
         uv_measurement_noise = gtsam.noiseModel.Isotropic.Sigma(2, 30)
         landmark_prior_noise = gtsam.noiseModel.Isotropic.Sigma(3, 0.1)
-        pose_prior_noise = gtsam.noiseModel.Diagonal.Sigmas(np.ones(6)*0.1)
+        pose_prior_noise = gtsam.noiseModel.Diagonal.Sigmas(np.ones(6)*1)
 
         # create projection factors
         for frame_id, curr_frame in self.frontend.frames.items():
+            curr_frame = self.frontend.frames[frame_id]
             for landmark_id in curr_frame.observed_landmark_id:
                 landmark = self.frontend.landmarks[landmark_id]
-                if landmark.frames[0]==frame_id:
-                    initial_estimate.insert(L(landmark_id), gtsam.Point3(*landmark.xyz[frame_id]))
-                    graph.push_back(gtsam.PriorFactorPoint3(
-                        L(landmark_id), gtsam.Point3(*landmark.xyz[frame_id]), landmark_prior_noise
+                if use_smart_factor:
+                    if landmark.frames[0]==frame_id:
+                        factor = gtsam.SmartProjectionPose3Factor(uv_measurement_noise, K)
+                        smart_factors[landmark_id] = factor
+                        graph.add(factor)
+                    else:
+                        factor = smart_factors[landmark_id]
+                    factor.add(np.array(landmark.uv[frame_id]), X(frame_id))
+                else:
+                    if landmark.frames[0]==frame_id:
+                        initial_estimate.insert(L(landmark_id), gtsam.Point3(*landmark.xyz[frame_id]))
+                        graph.push_back(gtsam.PriorFactorPoint3(
+                            L(landmark_id), gtsam.Point3(*landmark.xyz[frame_id]), landmark_prior_noise
+                        ))
+                    graph.push_back(gtsam.GenericProjectionFactorCal3_S2(
+                        np.array(landmark.uv[frame_id]), uv_measurement_noise, X(frame_id),
+                        L(landmark_id), K
                     ))
-                graph.push_back(gtsam.GenericProjectionFactorCal3_S2(
-                    np.array(landmark.uv[frame_id]), uv_measurement_noise, X(frame_id),
-                    L(landmark_id), K
-                ))
             pose = gtsam.Pose3(
                 gtsam.Rot3(curr_frame.rotation),
                 gtsam.Point3(curr_frame.translation.flatten())
@@ -58,7 +72,8 @@ class GTSAM_Backend():
         # plot
         self.plot()
 
-    def plot(self):
+    def plot(self, plot_input_traj=True, plot_estimated_traj=True, plot_gt_traj=False):
+        # setup canvas
         # fignum = 0
         # fig = plt.figure(fignum)
         # ax = plt.axes(projection='3d')
@@ -70,22 +85,41 @@ class GTSAM_Backend():
         # axes.set_xlim3d(-1, 1)
         # axes.set_ylim3d(-1, 1)
         # axes.set_zlim3d(-1, 1)
-        estimated_traj = np.zeros([3, len(self.frontend.frames)])
-        input_traj = np.zeros([3, len(self.frontend.frames)])
-        for frame_id, curr_frame in self.frontend.frames.items():
-            estimated_traj[:, frame_id] = self.current_estimate.atPose3(X(frame_id)).translation()
-            input_traj[:, frame_id] = curr_frame.translation.flatten()
-        ax.plot3D(estimated_traj[0,:], estimated_traj[1,:], estimated_traj[2,:], 'red', label='estimated')
-        ax.plot3D(input_traj[0,:], input_traj[1,:], input_traj[2,:], 'blue', label='input')
+        first_id = next(iter(self.frontend.frames))
+
+        # plot input_traj
+        if plot_input_traj:
+            traj = np.zeros([3, len(self.frontend.frames)])
+            for frame_id, curr_frame in self.frontend.frames.items():
+                traj[:, frame_id-first_id] = curr_frame.translation.flatten()
+            ax.plot3D(traj[0,:], traj[1,:], traj[2,:], 'blue', label='input')
+
+        # plot estimated_traj
+        if plot_estimated_traj:
+            traj = np.zeros([3, len(self.frontend.frames)])
+            for frame_id, curr_frame in self.frontend.frames.items():
+                traj[:, frame_id-first_id] = self.current_estimate.atPose3(X(frame_id)).translation()
+            ax.plot3D(traj[0,:], traj[1,:], traj[2,:], 'red', label='estimated')
+
+        # show the plot
         ax.legend()
         plt.show()
         # plt.pause(100)
 
 if __name__ == '__main__':
-    dataset_type = 'soulcity'
-    dataset_folder = os.path.expanduser('~/Projects/curly_slam/data/soulcity')
-    frontend = FrontendEvaluate(dataset_type, dataset_folder, start=0)
-    frontend.load_dataset(os.path.expanduser('~/Projects/curly_slam/data/curly_frontend/curly.txt'))
+    dataset_type = 'tartanair'
+    dataset_folder = os.path.expanduser('~/Projects/curly_slam/data/tartanair/scenes/seasidetown/Easy/P000')
+    frontend_file = os.path.expanduser('~/Projects/curly_slam/data/curly_frontend/curly_tartanair_seasidetown_000.txt')
+    dataset_path = {
+        'depth': dataset_folder+'/depth_image',
+        'color': dataset_folder+'/image_left',
+        'frontend': frontend_file,
+        'gt_traj': dataset_folder+'/pose_left.txt',
+        'odom_traj': dataset_folder+'/pose_left_noisy.txt'
+    }
+    frontend = FrontendEvaluate(dataset_type, dataset_path)
+    frontend.load_dataset(start=0, end=30)
+
     backend = GTSAM_Backend(frontend)
     backend.optimize()
     print("EOF")
--- noisy_traj.py
+++ noisy_traj.py
@@ -4,6 +4,7 @@ Adding Gaussian noise to the trajectory.
 import os
 from scipy.spatial.transform import Rotation as R
 import numpy as np
+import matplotlib.pyplot as plt
 
 class NoisyTraj():
     def __init__(self, input_file):
@@ -21,7 +22,7 @@ class NoisyTraj():
                 self.poses[frame_id] = (rot, pos)
                 frame_id += 1
     
-    def add_noise(self, pos_sigma, rot_sigma, output_file):
+    def add_noise(self, mean_pos_sigma, mean_rot_sigma, pos_sigma, rot_sigma, output_file, plot=True):
         curr_rot = self.poses[0][0]
         curr_pos = self.poses[0][1]
         output = open(output_file, 'w')
@@ -31,20 +32,41 @@ class NoisyTraj():
         # iter over poses
         rot_euler_noise = np.zeros(3)
         pos_noise = np.zeros(3)
+        mean_euler = np.random.normal(0, mean_rot_sigma, 3)
+        mean_pos = np.random.normal(0, mean_pos_sigma, 3)
+        input_traj = []
+        noisy_traj = []
         for frame_id in range(1, len(self.poses)):
             # add noise
             curr_rot = self.poses[frame_id][0]
             curr_pos = self.poses[frame_id][1]
-            rot_euler_noise += np.random.normal(0, rot_sigma, 3)
-            pos_noise += np.random.normal(0, pos_sigma, 3)
+            rot_euler_noise += np.random.normal(mean_euler, rot_sigma, 3)
+            pos_noise += np.random.normal(mean_pos, pos_sigma, 3)
             curr_rot_noisy = R.from_euler('zyx', curr_rot.as_euler('zyx')+rot_euler_noise)
             curr_pos_noisy = R.from_euler('zyx', rot_euler_noise).as_matrix()@pos_noise+curr_pos
+            input_traj += [curr_pos]
+            noisy_traj += [curr_pos_noisy]
             # save traj
             line = np.concatenate([curr_pos_noisy, curr_rot_noisy.as_quat()])
             output.write(' '.join([str(x) for x in line])+'\n')
+        if plot:
+            fig, ax = plt.subplots(subplot_kw=dict(projection="3d"))
+            ax.set_xlabel('x')
+            ax.set_ylabel('y')
+            ax.set_zlabel('z')
+            traj = np.zeros([3, len(input_traj)])
+            for i, pos in enumerate(input_traj):
+                traj[:, i] = pos
+            ax.plot3D(traj[0,:], traj[1,:], traj[2,:], 'blue', label='input')
+            traj = np.zeros([3, len(noisy_traj)])
+            for i, pos in enumerate(noisy_traj):
+                traj[:, i] = pos
+            ax.plot3D(traj[0,:], traj[1,:], traj[2,:], 'red', label='noisy')
+            ax.legend()
+            plt.show()
 
 if __name__ == '__main__':
-    dataset_folder = os.path.expanduser('~/Projects/curly_slam/data/soulcity/')
-    nt = NoisyTraj(dataset_folder+'pose_left.txt')
-    nt.add_noise(0.1, 0.05, dataset_folder+'pose_left_noisy.txt')
+    dataset_folder = os.path.expanduser('~/Projects/curly_slam/data/tartanair/scenes/seasidetown/Easy/P000')
+    nt = NoisyTraj(dataset_folder+'/pose_left.txt')
+    nt.add_noise(0.01, 0.01, 0.003, 0.01, dataset_folder+'/pose_left_noisy.txt')
     print("EOF")
